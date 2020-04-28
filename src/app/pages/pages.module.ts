bf96830de4bd796347a8a432c923d1130fa49682
AtuelGL <atuelgl@gmail.com>
2020-04-27 23:28:05 -0300

    Pre tarea

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,6 +5,10 @@ import { SharedModule } from '../shared/shared.module';
 
 import { FormsModule } from '@angular/forms';
 
+// ng2 CHarts
+import { ChartsModule } from 'ng2-charts';
+
+
 import { PagesComponent } from './pages.component';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -34,7 +38,8 @@ import { IncrementerComponent } from '../components/incrementer/incrementer.comp
     imports: [
         SharedModule,
         PAGES_ROUTES,
-        FormsModule
+        FormsModule,
+        ChartsModule
     ]
 })
 export class PagesModule {}
